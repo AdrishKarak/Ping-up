c3beda0e6649030bd73562cf105101a8ea9105b1
Adrish <runakarak@gmail.com>
2025-11-05 23:11:28 +0530

    foder

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from "./pages/Login.jsx";
+import Messages from "./pages/Messages.jsx";
+import Feed from "./pages/Feed.jsx";
+import ChatBox from "./pages/ChatBox.jsx";
+import Connections from "./pages/Connections.jsx";
+import Discover from "./pages/Discover.jsx";
+import Profile from "./pages/Profile.jsx";
+import CreatePost from "./pages/CreatePost.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    return (
+        <div>
+    <Routes>
+        <Route path="/" element={<Login/>}>
+         < Route index element={<Feed/>} />
+            <Route path='messages' element={<Messages/>} />
+            <Route path='messages/:userid' element={<ChatBox/>} />
+            <Route path='connections' element={<Connections/>} />
+            <Route path='discover' element={<Discover/>} />
+            <Route path='profile' element={<Profile/>} />
+            <Route path='profile/:profileId' element={<Profile/>} />
+            <Route path='create-post' element={<CreatePost/>} />
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+        </Route>
+    </Routes>
+        </div>
+    );
+};
 
-export default App
+export default App;
