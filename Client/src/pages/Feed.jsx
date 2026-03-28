bd7fcaf503858f962543e0a47c404194d60144d3
Adrish <runakarak@gmail.com>
2026-03-28 14:01:31 +0530

    feed

--- Client/src/pages/Feed.jsx
+++ Client/src/pages/Feed.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { dummyPostsData } from '../assets/assets'
+import Loading from '../components/Loading';
+
 
 const Feed = () => {
-    return (
-        <div>Feed</div>
-    );
+    const [feeds, setFeeds] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const fetchFeeds = async () => {
+        setFeeds(dummyPostsData);
+    }
+
+    useEffect(() => {
+        fetchFeeds();
+    }, [])
+
+    return !loading ? (
+        <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+            {/*stories and post */}
+            <div>
+
+            </div>
+
+            {/*right side bar */}
+            <div>
+
+            </div>
+        </div>
+    ) : <Loading />
 };
 
 export default Feed;
