3bd70640fbdb3f5c12549697bf45f064ca8d7bc1
Adrish <runakarak@gmail.com>
2026-04-01 01:09:39 +0530

    hg

--- Client/src/pages/Profile.jsx
+++ Client/src/pages/Profile.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { dummyUserData, dummyPostsData } from '../assets/assets';
+import Loading from '../components/Loading';
 
 const Profile = () => {
-    return (
-        <div>Profile</div>
-    );
+    const { profileId } = useParams();
+    const [user, setUser] = useState(null);
+    const [posts, setPosts] = useState([]);
+    const [activeTab, setActiveTab] = useState('posts');
+    const [showEdit, setShowEdit] = useState('false');
+
+    const fetchUser = async () => {
+        setUser(dummyUserData);
+        setPosts(dummyPostsData);
+    }
+
+    useEffect(() => {
+        fetchUser();
+    }, []);
+
+    return user ? (
+        <div className='relative h-full overflow-y-scroll bg-gray-50 p-6'>
+            <div className='max-w-3xl mx-auto'>
+                {/* Profile Card */}
+                <div className='bg-white rounded-2xl shadow overflow-hidden'>
+
+                </div>
+            </div>
+        </div>
+    ) : (<Loading />)
 };
 
 export default Profile;
