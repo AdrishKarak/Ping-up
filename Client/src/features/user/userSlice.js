1905b0e794c748f8fa03b9f80c3a79ddf5d87771
Adrish <runakarak@gmail.com>
2026-04-18 00:38:58 +0530

    dfgh

---
+++ Client/src/features/user/userSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit"
+import toast from "react-hot-toast"
+
+
+const initialState = {
+    value: null
+}
+
+export const fetchUser = createAsyncThunk('user/fetchUser', async (token) => {
+    const { data } = await api.get('/api/user/data', {
+        headers: { Authorization: `Bearer ${token}` }
+    })
+    return data.success ? data.user : null
+})
+
+export const updateUser = createAsyncThunk('user/update', async ({ userData, token }) => {
+    const { data } = await api.post('/api/user/update', userData, {
+        headers: { Authorization: `Bearer ${token}` }
+    })
+    if (data.success) {
+        toast.success(data.message)
+        return data.user
+    } else {
+        toast.error(data.message)
+        return null
+    }
+})
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+
+    }
+})
+
+export default userSlice.reducer
